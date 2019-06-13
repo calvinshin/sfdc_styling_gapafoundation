3dc157d0576e3596f7e6ccca64d10797b82ba9be
Calvin Shin <calvin.shin@gmail.com>
2019-06-12 19:31:25 -0700

    Made changes to the CSS styling, trying to remove the hover effect on specific tables

---
+++ js.js
@@ -0,0 +1,7 @@
+console.log("test");
+
+salesforce = document.getElementById("salesforce");
+
+salesforce.onload = function() {
+    document.getElementById("jobContainer").style.backgroundColor = "green";
+}
